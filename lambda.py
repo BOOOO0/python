973dd2fa91fa7f23082294856a9fefe295426c16
BOOOO0 <sunarol@naver.com>
2024-07-01 16:41:52 +0900

    함수 표현식

--- lambda.py
+++ lambda.py
@@ -11,3 +11,9 @@ print(reduce(lambda x, y: x + y, ls, 0))
 print(reduce(lambda x, y: x + y, ls, 10))
 
 print("true" if True else "false")
+
+ranked_users = ['jon', 'bob', 'jane', 'alice', 'chris']
+
+user_details = [{'name': x, 'rank': i+1} for i, x in enumerate(ranked_users)]
+
+print(user_details)
